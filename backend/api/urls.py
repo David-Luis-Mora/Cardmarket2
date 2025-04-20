eca514265f6bc2e0b6942a9935d5a8e3114599a7
David-Luis-Mora <davidluismora@gmail.com>
2025-04-20 16:52:26 +0100

    cambio en la url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('card/<str:slug>/', views.card_detail, name='game-detail'),
     path('users/login/', views.user_login, name='user-login'),
     path('users/signup/', views.user_signup, name='user-signup'),
-    path('users/edit/', views.user_signup, name='edit-profile'),
+    path('users/edit/', views.edit_profile, name='edit-profile'),
 ]
