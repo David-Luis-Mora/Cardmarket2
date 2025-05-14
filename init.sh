bc299cabd0a79184d66ef4d87a1c6327a45b6aa8
David-Luis-Mora <161330695+David-Luis-Mora@users.noreply.github.com>
2025-05-14 21:32:15 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,10 @@ npm install
 
 npm run dev &
 
+python ./backend/manage.py makemigrations
+
+python ./backend/manage.py migrate
+
+python ./backend/script_final.py
+
 python ./backend/manage.py runserver
