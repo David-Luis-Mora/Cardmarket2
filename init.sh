373e1bfef37edfb648ee7079d8bc7443969db510
David-Luis-Mora <161330695+David-Luis-Mora@users.noreply.github.com>
2025-05-14 22:30:06 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-cd fronted/
-npm install
-npm run dev &
+# Iniciar el frontend (Node.js)
+cd fronted/               # Cambiar al directorio del frontend
+npm install               # Instalar las dependencias del frontend
+npm run dev -- --port 8000 &   # Ejecutar el servidor de desarrollo en segundo plano en el puerto 8000
+
+# Regresar al directorio raíz
 cd ../
 
+# Ejecutar comandos para Django
+echo "Iniciando el backend (Django)"
 python ./backend/manage.py makemigrations
-
 python ./backend/manage.py migrate
+# python ./backend/script_final.py
 
-python ./backend/script_final.py
-
-python ./backend/manage.py runserver
+# Iniciar el servidor de Django (en el puerto 8000)
+python ./backend/manage.py runserver 0.0.0.0:8000
