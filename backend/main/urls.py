caff4b039e59d566d14d5954bfed6de924258501
David-Luis-Mora <161330695+David-Luis-Mora@users.noreply.github.com>
2025-05-15 00:43:47 +0100

    Update urls.py

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
-from ..api import views
+from api import views
 
 urlpatterns = [
     path('', views.redirect_to_vue_dev),  # redirige raíz a Vue en modo dev
